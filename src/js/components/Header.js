74c8576504d37b9903e844115cc4748d44fb96d3
unknown <johnpatrickdfelicia.com>
2024-10-05 22:29:32 +0800

    Header added user name, user background, user description.

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header'>
+      <section>
+        <div className='header-content'>
+          <h1>John Patrick Felicia</h1>
+          <h3>
+            Front-End Developer Specializing in Clean and<br/>
+            Responsive Web Solutions.
+          </h3>
+          <p>
+            “I help agencies and startups build user-focused,<br/>
+            scalable, and responsive websites using modern front-end<br/>
+            technologies.”
+          </p>
+        </div>
+        <div className='header-cta'>
+            <button>View my projects</button>
+            <button>Let's Collaborate</button>
+          </div>
+      </section>
+    </header>
   )
 }
 
---
+++ src/js/components/Nav.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+      <a href=''>
+        <img src='' alt='' />
+      </a>
+
+      {/* mobile navigation view */}
+
+      {/* navigation items */}
+      <ul>
+        <li>
+          <a href=''></a>
+        </li>
+        <li>
+          <a href=''></a>
+        </li>
+        <li>
+          <a href=''></a>
+        </li>
+        <li>
+          <a href=''></a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
