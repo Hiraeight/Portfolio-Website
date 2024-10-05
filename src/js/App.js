241f6bb663c8a1339d409237f56d4c243d8b8339
unknown <johnpatrickdfelicia.com>
2024-10-05 14:00:16 +0800

    Portfolio sections added. Header, SkillSet, Projects, AboutMe, CallToAction and Footer

---
+++ src/js/App.js
@@ -0,0 +1,22 @@
+import '../css/App.css';
+import Header from './components/Header';
+import SkillSet from './components/SkillSet';
+import Projects from './components/Projects';
+import AboutMe from './components/AboutMe';
+import CatllToAction from './components/CatllToAction';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <SkillSet />
+      <Projects />
+      <AboutMe />
+      <CatllToAction />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/js/components/CatllToAction.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CatllToAction = () => {
+  return (
+    <div>CatllToAction</div>
+  )
+}
+
+export default CatllToAction
