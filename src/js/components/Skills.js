cf9c6bed2f494cf8854759784371c25c5c2ccc59
unknown <johnpatrickdfelicia.com>
2024-10-11 01:50:41 +0800

    Skillset section added. Applied responsiveness for header and skillset sections.

---
+++ src/js/components/Skills.js
@@ -0,0 +1,75 @@
+import lang1 from '../../images/skillset/language/javascript.png';
+import lang2 from '../../images/skillset/language/html.png';
+import lang3 from '../../images/skillset/language/css.png';
+import lang4 from '../../images/blank.png';
+import lang5 from '../../images/blank.png';
+import lang6 from '../../images/blank.png';
+
+import frame1 from '../../images/skillset/frameworks/react.png';
+import frame2 from '../../images/skillset/frameworks/native.png';
+import frame3 from '../../images/skillset/frameworks/bootstrap.png';
+import frame4 from '../../images/blank.png';
+import frame5 from '../../images/blank.png';
+import frame6 from '../../images/blank.png';
+
+import tool1 from '../../images/skillset/tools/git.png';
+import tool2 from '../../images/skillset/tools/github.png';
+import tool3 from '../../images/skillset/tools/visual studio.png';
+import tool4 from '../../images/skillset/tools/npm.png';
+import tool5 from '../../images/skillset/tools/figma.png';
+import tool6 from '../../images/skillset/tools/webflow.jpeg';
+
+import other1 from '../../images/skillset/others/clickup.jpeg';
+import other2 from '../../images/skillset/others/slack.jpeg';
+import other3 from '../../images/skillset/others/memberstack.jpeg';
+import other4 from '../../images/skillset/others/zapier.jpeg';
+import other5 from '../../images/skillset/others/airtable.jpeg';
+import other6 from '../../images/skillset/others/jira.png';
+
+const skills = [
+    {
+        id: 1,
+        category: "Languages",
+        image1: lang1,
+        image2: lang2,
+        image3: lang3,
+        image4: lang4,
+        image5: lang5,
+        image6: lang6,
+    },
+    
+    {
+        id: 2,
+        category: "Frameworks",
+        image1: frame1,
+        image2: frame2,
+        image3: frame3,
+        image4: frame4,
+        image5: frame5,
+        image6: frame6,
+    },
+    
+    {
+        id: 3,
+        category: "Tools",
+        image1: tool1,
+        image2: tool2,
+        image3: tool3,
+        image4: tool4,
+        image5: tool5,
+        image6: tool6,
+    },
+    
+    {
+        id: 4,
+        category: "Others",
+        image1: other1,
+        image2: other2,
+        image3: other3,
+        image4: other4,
+        image5: other5,
+        image6: other6,
+    },
+]
+
+export default skills
